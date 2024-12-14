85e35add891e63c25fe51095860f2e46101c558f
ABHIJIT\gadak <gadakhabhijit@gmail.com>
2024-12-14 23:01:50 +0530

    changes for cors

--- server/index.js
+++ server/index.js
@@ -9,12 +9,19 @@ require('dotenv').config();
 require("./config/db");
 const PORT = process.env.PORT || 8080;
 
+const corsOptions = {
+  origin: 'https://auth-practice-client.vercel.app', // replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // if you want to allow cookies or authentication headers
+};
+
 app.get('/ping', (req, res)=>{
     res.send("pong");
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors(corsOptions));
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
